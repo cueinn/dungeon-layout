c42020cf00813d10a8994b343e05c47808cab10b
Felipe Silva <felipesilva@MacBook-Pro-de-Felipe.local>
2021-10-29 11:22:42 -0300

    Adiciona função que gera 4 salas aleatórias

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('conteiner');
 const resetButton = document.getElementById('resetButton');
+const randButton = document.getElementById('randButton');
 const rooms = Array.from(document.querySelectorAll('.room'));
 
 function highlightRoom(event) {
@@ -14,8 +15,16 @@ function resetRooms(event) {
   });
 }
 
+function randomize() {
+  for (let i = 1; i < 5; i++) {
+    var randRoom = rooms[Math.floor(Math.random()*rooms.length)];
+    randRoom.classList.add('active');
+  }
+}
+
 container.addEventListener('click', highlightRoom);
 resetButton.addEventListener('click', resetRooms);
+randButton.addEventListener('click', randomize);
 document.addEventListener('keydown', function(e){
   if (e.key === 'r') {
     resetRooms()
